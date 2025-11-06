ddf3db94d25c7f9e115a4c029459d275c3bf9afc
uktveris <marius.uktveris@gmail.com>
2025-11-06 20:15:59 +0100

    added debug logging in health check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,7 @@ router.use("/users", userRoutes);
 router.use("/turn-servers", tokenRoutes);
 
 router.get("/health", (req, res) => {
+  console.log("executing backend health check:", Date.now());
   res.json({ status: "backend is healthy" });
 });
 
